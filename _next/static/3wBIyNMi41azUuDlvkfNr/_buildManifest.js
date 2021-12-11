1caf700d8543529a1c63741ba0aeb3cbb790af89
marushosummers <marushosummers@users.noreply.github.com>
2021-12-11 06:49:31 +0000

    deploy: 2c6b769efbab437fdcfca34a7e07283ad10bc1d2

---
+++ _next/static/3wBIyNMi41azUuDlvkfNr/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/cbb75d1bbdf0c9f1.css","static/chunks/pages/index-f2d513e6c9b4e338.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
