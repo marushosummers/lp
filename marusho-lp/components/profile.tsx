f567dbfbfccb1d7c6430d1e67e2c694fc2967ad0
marusho <mrsho34@gmail.com>
2021-12-11 06:30:11 +0000

    update avatar

--- marusho-lp/components/profile.tsx
+++ marusho-lp/components/profile.tsx
@@ -2,7 +2,7 @@ const Profile = () => {
   return (
     <section className="flex flex-col md:flex-row justify-center mt-16 mb-16 md:mb-12">
       <div className="flex justify-center p-4">
-        <img src={"/assets/icon/icon_400x400.jpg"} className="w-36 h-36 rounded-full" alt={"icon"} />
+        <img src={"/avatar.jpg"} className="w-36 h-36 rounded-full" alt={"icon"} />
       </div>
       <div className="flex justify-center p-4 items-center">
         <p className="text-xl md:text-xl tracking-tighter leading-tight">Web Developer</p>
